d761c94224186e0eef7a99230561fc7b8274bbd6
Priyanshu <priyanshusingh.0601@gmai.com>
2025-06-16 19:05:40 +0530

    add feedback

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { interviewer } from '@/constants';
+import { createFeedback } from '@/lib/actions/general.actions';
 import { cn } from '@/lib/utils';
 import { vapi } from '@/lib/vapi.sdk';
 import Image from 'next/image'
@@ -64,10 +65,11 @@ export default function Agent({userName, userId, type, interviewId, questions, f
     const handleGenerateFeedback = async (messages: SavedMessage[]) => {
         console.log('Generate feedback here');
 
-        const { success, id } = {
-            success: true,
-            id: 'feedback-id'
-        }
+        const { success, feedbackId: id } = await createFeedback({
+          interviewId: interviewId!,
+          userId: userId!,
+          transcript: messages  
+        })
         if(success && id){
             router.push(`/interview/${interviewId}/feedback`);
         } else {
